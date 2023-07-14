6940566f73f066468a11ee6fc60d359c1c1e96fc
María Luisa Martínez <mluisamartinezc@gmail.com>
2023-07-14 13:23:59 +0200

    error handling

---
+++ src/main/java/com/example/movies/controller/GlobalErrorAttributes.java
@@ -0,0 +1,9 @@
+package com.example.movies.controller;
+
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GlobalErrorAttributes extends DefaultErrorAttributes {
+
+}
